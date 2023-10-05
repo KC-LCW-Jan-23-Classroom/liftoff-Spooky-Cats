1ba6e69a1c6c59a51e57e7123831a59de2177213
Josephina Lacy <josephinalacy@gmail.com>
2023-10-04 20:24:53 -0500

    spring

---
+++ back-end/feral-cat-tracker/src/main/java/com/spookycats/feralcattracker/FeralCatTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.spookycats.feralcattracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeralCatTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeralCatTrackerApplication.class, args);
+	}
+
+}
