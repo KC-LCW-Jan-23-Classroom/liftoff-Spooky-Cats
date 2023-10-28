fa28b3c3769dddc1f328ae09f651ffed2baab2c8
jlacy <josephinalacy@gmail.com>
2023-10-27 19:05:57 -0500

    backend working with catData DTO, Model, Service, and Controller

---
+++ feral-cat-tracker/src/main/java/com/spookycats/feralcattracker/controllers/CatDataController.java
@@ -0,0 +1,48 @@
+package com.spookycats.feralcattracker.controllers;
+
+import com.spookycats.feralcattracker.models.dto.CatDataFormDTO;
+import com.spookycats.feralcattracker.services.CatDataService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CatDataController {
+
+    @Autowired
+    CatDataService catDataService;
+
+
+    //TODO: GET MAPPING, RETURN ALL CATS. FILTER CATS BY SEARCHING
+
+    @PostMapping("/log")
+    public ResponseEntity<String> processCatDataForm(@RequestBody CatDataFormDTO catDataFormDTO,
+                                                      HttpServletRequest request)  {
+        ResponseEntity response;
+
+        if(catDataFormDTO.getMicrochipNumber() == null)
+        {
+            response = ResponseEntity.status(HttpStatus.BAD_REQUEST).body("All cats require a Microchip number.");
+            return response;
+        }
+
+        Boolean exisitingCat = catDataService.findExistingCat(catDataFormDTO);
+
+        if(exisitingCat == true){
+            catDataService.updateCat(catDataFormDTO);
+            response = ResponseEntity.status(HttpStatus.OK).body("[TODO]Existing Cat has been updated.");
+        } else {
+            catDataService.addNewCat(catDataFormDTO);
+            response = ResponseEntity.status(HttpStatus.OK).body("New cat logged.");
+        }
+
+        return response;
+
+
+    }
+
+}
