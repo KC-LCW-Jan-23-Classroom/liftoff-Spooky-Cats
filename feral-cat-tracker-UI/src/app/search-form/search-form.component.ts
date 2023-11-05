63f64ab1d2769bdde8e37d8c1e813b1ba00c12a4
marysanders <lyremary@yahoo.com>
2023-11-05 14:05:29 -0600

    refinements

--- feral-cat-tracker-UI/src/app/search-form/search-form.component.ts
+++ feral-cat-tracker-UI/src/app/search-form/search-form.component.ts
@@ -8,10 +8,21 @@ import { Router } from '@angular/router';
 })
 export class SearchFormComponent {
   searchTerm!: string; 
+  queryMicrochip!: string; 
+  queryLocation!: string;
   constructor(private router: Router) {}
 
   searchCats({ searchTerm }: { searchTerm: string }) {
-    console.log(searchTerm);
-    this.router.navigate(['/results'], {queryParams: {query: searchTerm}});
+    let queryType: string;
+    if (this.queryMicrochip === 'microchip') {
+      queryType = 'microchip';
+    } else if (this.queryLocation === 'location') {
+      queryType = 'location';
+    } else {
+      queryType = 'all'; // Default to "all" if no radio button is selected
+    }
+    console.log(searchTerm + "Search Term");
+    console.log(queryType + "Query type")
+    this.router.navigate(['/results'], {queryParams: {query: searchTerm, queryType: queryType }});
   }
 }
