0632ffca50d766e92a183e5357e98696581f770a
jlacy <josephinalacy@gmail.com>
2023-11-26 00:28:59 -0600

    working cat map with markers for addresses converted to coordinates by google service. Map will automatically render based on furthers points each direction

---
+++ feral-cat-tracker-UI/src/app/models/marker.ts
@@ -0,0 +1,20 @@
+export class Marker {
+
+position: Position;
+
+    constructor(position: Position){
+        this.position;
+    }
+
+}
+
+export class Position {
+    lat: any;
+    lng: any;
+
+    constructor(lat: any, lng: any){
+        this.lat = lat;
+        this.lng = lng;
+    }
+
+}
